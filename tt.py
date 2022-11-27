7f383d5198ec029c5e4959a2df9a4db91a2e5d8f
YiChen <xx5244@gmail.com>
2022-11-27 14:03:44 +0800

    20221127_a

--- tt.py
+++ tt.py
@@ -1,15 +1,8 @@
-import tkinter as tk
-from tkinter import messagebox
+def test():
+    return 1, 2, 3
 
-win = tk.Tk()
-win.withdraw() # 隱藏根視窗
+a, b, c = test()
+print(a, b, c)
 
-messagebox.showinfo('title', 'content') # 顯示訊息對話框
-messagebox.showwarning('title', 'content') # 警告訊息對話框
-messagebox.showerror('title', 'content') # 錯誤訊息對話框
-messagebox.askokcancel('title', 'content') # 確定/取消訊息對話框
-messagebox.askquestion('title', 'content') # 是/否訊息對話框
-
-"""
-判斷使用者選擇確定/取消/是/否，只要把結果丟進變數裡，判斷變數內容即可
-"""
+d, _, _ = test()
+print(d)
